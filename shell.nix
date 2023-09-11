ee7afc6dea22c1cf513ff8892ad3f92bec1f4ec6
Răzvan Flavius Panda <razvan.panda@gmail.com>
2023-09-11 05:03:45 +0300

    #

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@ let pkgs = import <nixpkgs> { };
 in
 pkgs.mkShell {
   buildInputs = [
-    (pkgs.haskellPackages.ghcWithPackages (p: [ p.invariant p.hedgehog p.lens ]))
+    (pkgs.haskell.packages.ghc946.ghcWithPackages (p: [ p.invariant p.hedgehog p.lens ]))
     pkgs.cabal-install
     pkgs.ghcid
     pkgs.hlint
